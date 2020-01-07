1905bd18feb3a6c05b57a7d2f2ef2cf81c0b469d
victor-s-santos <victor.s.silva@unesp.br>
2020-01-07 17:08:15 -0300

    acrescentado a rota de busca pelo id, e refatoramento do código, reaproveitando o app.config de home

--- app.py
+++ app.py
@@ -4,28 +4,40 @@ app = Flask(__name__)
 
 businessman = [
     {
+        "id": 0,
         "name": "Luiz Barsi",
         "historic": "Considerado o maior investidor individual do Brasil, Luiz Barsi, agora com 80 anos, já foi engraxate. Começou sua carreira de investidor na década de 1960, comprando ações da Cesp. Segundo reportagem do jornal O Estado de S. Paulo de agosto de 2019, seu patrimônio chega a quase R$ 2 bilhões. Mesmo assim, mantém hábitos simples e anda de metrô pela cidade. Sua estratégia de investimento na bolsa de valores consiste em comprar ações que pagam bons dividendos e ficar com elas, independentemente da variação no preço dos papéis. Segundo ele, a ideia não é investir em ações da bolsa, mas comprar participações em boas empresas."
     },
     {
+        'id': 1,
         'name': 'Victor Adler',
         'historic': 'Victor Adler divide com Luiz Barsi e outro integrante desta lista, Lírio Parisotto, o posto de um dos maiores acionistas da Eternit, conhecida por ser uma boa pagadora de dividendos. Em 2019, o investidor anunciou que seu fundo alcançou neste ano 5,3% de participação acionária na empresa de telefonia Oi. Mais discreto do que a maioria dos seus pares, comunga com Barsi o desapreço pela renda fixa e a preferência pelo mercado acionário. Para o portal Exame, deixou claro também que o mercado imobiliário não era a sua praia. Considera altos os custos com advogados e corretores e prefere fugir da baixa liquidez do setor.'
     },
     {
+        'id': 2,
         'name': 'Eufrásia Teixeira Leite',
         'historic': 'Nascida em 1850, Eufrásia Teixeira Leite foi uma mulher à frente de seu tempo. Perdeu os pais em 1872 e passou, junto com a sua irmã Francisca Bernardina, a administrar a herança recebida. Conseguiu multiplicar sua fortuna e deixou, em testamento, um valor que poderia comprar 1.850 quilos de ouro, ao preço da época. Morreu em 1930 e hoje seria considerada bilionária. Deixou a maior parte da herança a instituições assistenciais e educacionais da cidade de Vassouras, no Rio de Janeiro. A riqueza também lhe garantiu independência para viver de acordo com as suas escolhas, sem prestar contas a ninguém. Foi assim que concedeu a própria mão em casamento ao abolicionista Joaquim Nabuco. Foram 14 anos de idas e vindas, mas o casamento jamais ocorreu.'
     },
     {
+        'id':3,
         'name': 'Guilherme Affonso Ferreira',
         'historic': 'Outro grande investidor, Guilherme Affonso Ferreira é filho de um colaborador da Caterpillar, de quem herdou o apreço pelo trabalho árduo. Fez a graduação em engenharia de produção pela Escola Politécnica da USP (Universidade de São Paulo) e depois passou um ano fazendo estágios em diversos países. Sua história está bastante ligada à da Caterpillar, uma fabricante de máquinas, motores e veículos pesados. Com as sobras de capital da empresa, ele começou a fazer investimentos em outros negócios, que tivessem um ciclo diferente do negócio principal. Assim, aproveitou para comprar outras empresas com dinheiro novo por meio do fundo que administra, o Teorema.'
     }
 ]
-
+app.config['JSON_AS_ASCII'] = False#evitando problemas com caracteres especiais
+app.config['JSON_SORT_KEYS'] = False#ignorando a ordem alfabética de posicionamento
 @app.route('/businessman', methods=['GET'])#desnecessário, já que o get é método padrão
 def home():
-    app.config['JSON_AS_ASCII'] = False#evitando problemas com caracteres especiais
-    app.config['JSON_SORT_KEYS'] = False#ignorando a ordem alfabética de posicionamento
+    #app.config['JSON_AS_ASCII'] = False
+    #app.config['JSON_SORT_KEYS'] = False
     return jsonify(businessman), 200
+
+@app.route('/businessman/<int:id>')
+def businessman_by_id(id):
+    for person in businessman:
+        if person['id'] == id:
+            return jsonify(person), 200 
+    return jsonify({"error": "id couldn't be found"})
 
 if __name__ == '__main__':
     app.run(debug=True)
