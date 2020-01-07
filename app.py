33e9611ea92610fe8b6796e666775daccfefd5b3
victor-s-santos <victor.s.silva@unesp.br>
2020-01-07 16:21:42 -0300

    definido a primeira rota

--- app.py
+++ app.py
@@ -2,5 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/', methods=['GET'])#desnecessário, já que o get é método padrão
+def home():
+    return "It's running as a charm", 200
+
 if __name__ == '__main__':
     app.run(debug=True)
